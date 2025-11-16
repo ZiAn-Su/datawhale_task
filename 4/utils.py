56f173e11859accf6db6b59bb3258b8c27d08a12
jtz <977905691@qq.com>
2025-11-16 22:01:21 +0800

    第七章

--- 4/utils.py
+++ 4/utils.py
@@ -16,9 +16,9 @@ class HelloAgentsLLM:
         """
         初始化客户端。优先使用传入参数，如果未提供，则从环境变量加载。
         """
-        self.model = model or os.getenv("MODEL_ID")
-        apiKey = apiKey or os.getenv("API_KEY")
-        baseUrl = baseUrl or os.getenv("BASE_URL")
+        self.model = model or os.getenv("LLM_MODEL_ID")
+        apiKey = apiKey or os.getenv("LLM_API_KEY")
+        baseUrl = baseUrl or os.getenv("LLM_BASE_URL")
         timeout = timeout or int(os.getenv("LLM_TIMEOUT", 60))
         
         if not all([self.model, apiKey, baseUrl]):
